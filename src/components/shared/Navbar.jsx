02337e1289857027b6389a80101c873f0a4993c5
Andrea Castro <ap.castrocastellon@gmail.com>
2025-03-15 02:03:04 -0300

    avance estilos

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,8 +1,24 @@
 const Navbar = () => {
   return (
-      <div className="navbar">
-        navbar
+    <div className="navbar">
+
+      <div className="navbar__body">
+        
+        <div className="navbar__logo">
+          <span>DisplayPlayground</span>
+        </div>
+
+        <div className="navbar__links">
+          <button>
+            flex
+          </button>
+          <button>
+            grid
+          </button>
+        </div>
+
       </div>
+    </div>
   )
 }
 export default Navbar
